f59eb0f81b420e09c9ca50a461dfcd37fa8dbafc
Tommy Ervin <tommy.ervin1@gmail.com>
2018-09-12 15:34:02 -0600

    Move and Rotate

--- 1610/Move/Assets/Move.cs
+++ 1610/Move/Assets/Move.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-	public floate Speed;
+	public CharacterController Character;
+	
+	public float Speed;
 	public Vector3 MoveSpeed;
 	public Vector3 RotateSpeed;
 
 	private void Update()
 	{
-
 		MoveSpeed.x = Speed*Input.GetAxis("Horizontal");
 		MoveSpeed *= Time.deltaTime;
-		
-		transform.Translate(MoveSpeed);
-		transform.Rotate(RotateSpeed);
 
+		Character.Move(MoveSpeed);
 	}
-
-
 }
