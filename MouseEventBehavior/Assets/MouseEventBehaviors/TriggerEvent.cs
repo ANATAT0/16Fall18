740b2abb7521296e1011ea76bf65500980edc808
Tommy Ervin <tommy.ervin1@gmail.com>
2018-09-26 15:44:23 -0600

    PowerUp

---
+++ MouseEventBehavior/Assets/MouseEventBehaviors/TriggerEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+
+	public UnityEvent Enable;
+	public UnityEvent Trigger;
+	
+	void OnEnable()
+	{
+		Enable.Invoke();
+	}
+
+	void OnTriggerEnter()
+	{
+		Trigger.Invoke();
+	}
+	
+}
