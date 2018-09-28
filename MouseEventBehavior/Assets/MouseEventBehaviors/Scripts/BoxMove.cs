dfd37c6c5b9259dd4c189e8b2ed14cb5d2080118
Tommy Ervin <tommy.ervin1@gmail.com>
2018-09-27 23:02:06 -0600

    PowerUp 2
    Added movement. Need to figure out how to add health/money when moving over sprites

---
+++ MouseEventBehavior/Assets/MouseEventBehaviors/Scripts/BoxMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BoxMove : MonoBehaviour
+{
+    public float Speed;
+    public Vector3 MoveSpeed;
+    public Vector3 RotateSpeed;
+
+    private void Update()
+    {
+        MoveSpeed.x = Speed*Input.GetAxis("Horizontal");
+        MoveSpeed.z = Speed*Input.GetAxis("Vertical");
+        
+        transform.Translate(MoveSpeed);
+        transform.Rotate(RotateSpeed);
+
+    }
+}
