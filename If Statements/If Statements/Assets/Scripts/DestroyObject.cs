14d344e08b47e8344992a768a689d2a08c9b8efa
Tommy Ervin <tommy.ervin1@gmail.com>
2018-10-08 15:25:10 -0600

    Bullet

---
+++ If Statements/If Statements/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour 
+{
+	IEnumerator Start () 
+	{
+		yield return new WaitForSeconds(5);
+		Destroy((gameObject));
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+}
