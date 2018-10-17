b810e33b807ca5be37311471f3dc9c221d9d9d7a
Tommy Ervin <tommy.ervin1@gmail.com>
2018-10-17 15:36:29 -0600

    HealthBar

---
+++ If Statements/If Statements/Assets/Scripts/ChangeValue/UpdateImageValue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateImageValue : MonoBehaviour
+{
+
+	public FloatData Data;
+	private Image image;
+	
+	
+	void Start ()
+	{
+		image = GetComponent<Image>();
+	}
+	
+	void Update ()
+	{
+		image.fillAmount = Data.Value;
+	}
+}
