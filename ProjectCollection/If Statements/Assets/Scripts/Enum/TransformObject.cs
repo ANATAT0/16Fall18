7160c368743e6304f25de6e41a0a211a0a85efef
Tommy Ervin <tommy.ervin1@gmail.com>
2018-10-21 21:32:23 -0600

    ProjecCollection
    Files weren't updating to Github, so I tried to make a new file to see if it will now update.

---
+++ ProjectCollection/If Statements/Assets/Scripts/Enum/TransformObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformObject : MonoBehaviour
+{
+
+	public Vector3 Movement;
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update ()
+	{
+		//transform.Translate(Movement);
+		transform.Rotate(Movement);
+	}
+}
