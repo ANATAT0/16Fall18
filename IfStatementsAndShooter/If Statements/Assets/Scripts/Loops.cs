fbf8a4cc433934d27b6b049a0e6c0599397eddf2
Tommy Ervin <tommy.ervin1@gmail.com>
2018-10-24 15:07:33 -0600

    Loops

---
+++ IfStatementsAndShooter/If Statements/Assets/Scripts/Loops.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+
+	public string[] Names;
+	public GameObject[] Things;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i <= 50; i++)
+		{
+			print(Names[i]);
+			
+		}
+
+		foreach (var thing in Things)
+		{
+			print(thing);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
