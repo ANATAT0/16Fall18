77362362c39356e28c715e189db268b5f2ff15d6
Tommy Ervin <tommy.ervin1@gmail.com>
2018-10-27 23:00:04 -0600

    InstancingBase

---
+++ IfStatementsAndShooter/If Statements/Assets/Scripts/InstancingBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+
+public class InstancingBase : ScriptableObject
+{
+	private Vector3 position;
+	
+	
+	public void InstanceObject (GameObject prefab)
+	{
+		position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		position.z = 0;
+		Instantiate(prefab, position, Quaternion.identity);
+	}
+
+}
