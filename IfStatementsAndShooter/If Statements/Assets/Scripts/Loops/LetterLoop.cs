3a3ff6db10800230b5113a277413fbdb9f88b4ff
Tommy Ervin <tommy.ervin1@gmail.com>
2018-11-01 23:08:02 -0600

    LetterLoops

---
+++ IfStatementsAndShooter/If Statements/Assets/Scripts/Loops/LetterLoop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterLoop : MonoBehaviour
+{
+	public string[] Alphabet;
+	
+	void Start () 
+	{
+		foreach (var letter in Alphabet)
+		{
+			print(letter);
+		}
+	}
+	
+}
