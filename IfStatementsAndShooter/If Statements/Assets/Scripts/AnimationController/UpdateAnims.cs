027f949f59a7a1df4014322ceb838a1fda7ebcde
Tommy Ervin <tommy.ervin1@gmail.com>
2018-11-14 15:16:38 -0700

    Anims

---
+++ IfStatementsAndShooter/If Statements/Assets/Scripts/AnimationController/UpdateAnims.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+private Animator anims;
+public float speed;
+
+	void Start (){
+	{
+		Animas = GetComponent<Animator>();
+	}
+
+
+	void Update () 
+	{
+		anims.SetFloat("Speed", speed);	
+	}
+}
