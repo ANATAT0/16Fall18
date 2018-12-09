0501dc8c3e180add5625f7a9f5a08ea36b202f46
Tommy Ervin <tommy.ervin1@gmail.com>
2018-12-09 10:37:56 -0700

    Made2DandDoubleJump

--- IfStatementsAndShooter/If Statements/Assets/Scripts/2DControllers/GingerbreadCharacterMover.cs
+++ IfStatementsAndShooter/If Statements/Assets/Scripts/2DControllers/GingerbreadCharacterMover.cs
@@ -4,26 +4,81 @@ using UnityEngine;
 
 public class GingerbreadCharacterMover : MonoBehaviour
 {
-	private float speed = 3.0f;
-	public float jumpspeed = 4.0f;
-	public float gravity = 9.81f;
+	public float Speed;
+	public float JumpForce;
 	private float moveInput;
 
 	private Rigidbody2D rb;
 	private Animator anim;
 
+	private bool facingRight = true;
+	
+	private bool isGrounded;
+	public Transform groundCheck;
+	public float checkRadius;
+	public LayerMask whatIsGround;
+
+	private int extraJumps;
+	public int extraJumpValue;
+
+
+
+
 	void Start()
 	{
+		extraJumps = extraJumpValue;
 		rb = GetComponent<Rigidbody2D>();
+
 		anim = GetComponent<Animator>();
 	}
 
 
 	void FixedUpdate()
 	{
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
 		anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
-		
+
 		moveInput = Input.GetAxis("Horizontal");
-		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+		rb.velocity = new Vector2(moveInput * Speed, rb.velocity.y);
+
+		if (facingRight == false && moveInput > 0)
+		{
+			Flip();
+		}
+		else if (facingRight == true && moveInput < 0)
+		{
+			Flip();
+		}
+	}
+
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 Scaler = transform.localScale;
+		Scaler.x *= -1;
+		transform.localScale = Scaler;
+	}
+	
+
+	void Update()
+	{
+		if (isGrounded == true)
+		{
+			extraJumps = extraJumpValue;
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+		{
+			rb.velocity = Vector2.up * JumpForce;
+            extraJumps--;
+		}
+		else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+		{
+			rb.velocity = Vector2.up * JumpForce;
+		}
+		
 	}
 }
