1861f145bfa1f401896e191045713b551b8f1b4b
Tommy Ervin <tommy.ervin1@gmail.com>
2018-12-10 20:59:23 -0700

    MadeFinalGameFolder

---
+++ IfStatementsAndShooter/If Statements/Assets/Scripts/FinalGame/HealthBar/CandyCornDmg.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyCornDmg : MonoBehaviour
+{
+
+	private GingerbreadCharacterMover player;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<GingerbreadCharacterMover>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.CompareTag("Player"))
+		{
+			player.Damage(2);
+		}
+	}
+}
