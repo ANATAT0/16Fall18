7bb000c9506f2f61fda87851d2239b94c8b5e19a
Tommy Ervin <tommy.ervin1@gmail.com>
2018-12-10 17:54:58 -0700

    ChuggingAway

---
+++ IfStatementsAndShooter/If Statements/Assets/Scripts/PowerUps/DonutPowerUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Globalization;
+using UnityEngine.Events;
+
+public class DonutPowerUp : MonoBehaviour
+{
+	public int donutValue = 5;
+	public static UnityAction<int> UpdateDonut;
+
+	private void OnTriggerEnter(Collider col)
+	{
+		UpdateDonut(donutValue);
+		gameObject.SetActive(false);
+	}
+}
+
